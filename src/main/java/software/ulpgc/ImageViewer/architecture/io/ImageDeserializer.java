0e2edb57a0518ad3bdd212bc1108d5ed29641369
TychoQS <tycho.quintana@gmail.com>
2025-01-03 15:24:47 +0000

    Unused import

--- src/main/java/software/ulpgc/ImageViewer/architecture/io/ImageDeserializer.java
+++ src/main/java/software/ulpgc/ImageViewer/architecture/io/ImageDeserializer.java
@@ -1,7 +1,5 @@
 package software.ulpgc.ImageViewer.architecture.io;
 
-import software.ulpgc.ImageViewer.architecture.model.Image;
-
 public interface ImageDeserializer {
     Object deserialize(byte[] content);
 }
