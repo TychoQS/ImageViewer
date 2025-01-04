aebf4060fef42e0ca65e78b1b6709809e86c18ff
TychoQS <tycho.quintana@gmail.com>
2025-01-04 15:05:36 +0000

    Registered what to do on drag and release to the display. Also adapted de signature of the display method, that was changed.

--- src/main/java/software/ulpgc/ImageViewer/architecture/control/ImagePresenter.java
+++ src/main/java/software/ulpgc/ImageViewer/architecture/control/ImagePresenter.java
@@ -1,6 +1,5 @@
 package software.ulpgc.ImageViewer.architecture.control;
 
-import software.ulpgc.ImageViewer.architecture.io.FileImageLoader;
 import software.ulpgc.ImageViewer.architecture.model.Image;
 import software.ulpgc.ImageViewer.architecture.view.ImageDisplay;
 
@@ -10,11 +9,33 @@ public class ImagePresenter {
 
     public ImagePresenter(ImageDisplay display) {
         this.display = display;
+        this.display.onDragging(this::drag);
+        this.display.onReleasing(this::release);
+    }
+
+    private void drag(int offset) {
+        display.display(image, offset);
+        if (offset > 0) {
+            display.display(image.previous(), offset - display.getWidth());
+        } else {
+            display.display(image.next(), offset + display.getWidth());
+        }
+    }
+
+    private void release(int offset) {
+        if (Math.abs(offset) >= display.getWidth() / 2) {
+          image = offset > 0 ? image.previous() : image.next();
+        }
+        repaint();
+    }
+
+    private void repaint() {
+        this.display.display(image, 0);
     }
 
     public void display(Image image) {
         this.image = image;
-        display.display(image);
+        display.display(image, 0);
     }
 
     public void displayNext() {
