517c9e8b421db813bb2b692dce8630a3ccb752d6
TychoQS <tycho.quintana@gmail.com>
2025-01-04 15:03:04 +0000

    Interfaces to adapt graphical library events with method to register the action to do added. Also getWidth method added so we can call it with an instance of the interface.

--- src/main/java/software/ulpgc/ImageViewer/architecture/view/ImageDisplay.java
+++ src/main/java/software/ulpgc/ImageViewer/architecture/view/ImageDisplay.java
@@ -3,5 +3,16 @@ package software.ulpgc.ImageViewer.architecture.view;
 import software.ulpgc.ImageViewer.architecture.model.Image;
 
 public interface ImageDisplay {
-    void display(Image image);
+    void display(Image image, int offset);
+    void onReleasing(Released released);
+    void onDragging(Dragged dragged);
+    int getWidth();
+    interface Dragged {
+        Dragged Null = position -> {};
+        void position (int position);
+    }
+    interface Released {
+        Released Null = position -> {};
+        void position (int position);
+    }
 }
