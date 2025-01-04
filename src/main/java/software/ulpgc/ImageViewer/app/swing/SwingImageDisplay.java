e29ba7ecca93c2e588d1bbbd177aa48d3dbba675
TychoQS <tycho.quintana@gmail.com>
2025-01-04 15:04:29 +0000

    Added action listener to register the drag and release of the mouse over the image displayed. Also implemented new methods of the interface

--- src/main/java/software/ulpgc/ImageViewer/app/swing/SwingImageDisplay.java
+++ src/main/java/software/ulpgc/ImageViewer/app/swing/SwingImageDisplay.java
@@ -7,6 +7,9 @@ import software.ulpgc.ImageViewer.architecture.view.ViewPort;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +17,54 @@ import java.util.List;
 public class SwingImageDisplay extends JPanel implements ImageDisplay {
     private final ImageDeserializer deserializer;
     private final List<PaintOrder> paintOrders;
+    private Released released;
+    private Dragged dragged;
+    private int initialDraggingPosition;
 
     public SwingImageDisplay(ImageDeserializer deserializer) {
         this.deserializer = deserializer;
         this.paintOrders = new ArrayList<>();
+        dragged = Dragged.Null;
+        released = Released.Null;
+        this.addMouseListener(createMouseListener());
+        this.addMouseMotionListener(createMouseMotionListener());
     }
+
+    private MouseMotionListener createMouseMotionListener() {
+        return new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                dragged.position(e.getX() - initialDraggingPosition);
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {}
+        };
+    }
+
+    private MouseListener createMouseListener() {
+        return new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {}
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                initialDraggingPosition = e.getX();
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                released.position(e.getX() - initialDraggingPosition);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+
+            @Override
+            public void mouseExited(MouseEvent e) {}
+        };
+    }
+
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -40,10 +86,20 @@ public class SwingImageDisplay extends JPanel implements ImageDisplay {
     }
 
     @Override
-    public void display(Image image) {
+    public void display(Image image, int offset) {
         paintOrders.clear();
         paintOrders.add(getPaintOrderFrom(image));
         repaint();
+    }
+
+    @Override
+    public void onReleasing(Released released) {
+        this.released = released != null ? released : Released.Null;
+    }
+
+    @Override
+    public void onDragging(Dragged dragged) {
+        this.dragged = dragged != null ? dragged : Dragged.Null;
     }
 
     private java.awt.Image deserialize(byte[] content) {
