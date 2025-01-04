9fd38cbd4bf7063a6ee980483b82eac7258eefae
TychoQS <tycho.quintana@gmail.com>
2025-01-04 13:28:17 +0000

    Now presenter handles the image that is being displayed.

--- src/main/java/software/ulpgc/ImageViewer/architecture/control/ImagePresenter.java
+++ src/main/java/software/ulpgc/ImageViewer/architecture/control/ImagePresenter.java
@@ -1,24 +1,28 @@
 package software.ulpgc.ImageViewer.architecture.control;
 
+import software.ulpgc.ImageViewer.architecture.io.FileImageLoader;
 import software.ulpgc.ImageViewer.architecture.model.Image;
 import software.ulpgc.ImageViewer.architecture.view.ImageDisplay;
 
 public class ImagePresenter {
     private final ImageDisplay display;
+    private Image image;
 
     public ImagePresenter(ImageDisplay display) {
         this.display = display;
     }
 
     public void display(Image image) {
+        this.image = image;
         display.display(image);
     }
 
     public void displayNext() {
-        display.displayNext();
+        display(image.next());
     }
 
     public void displayPrevious() {
-        display.displayPrevious();
+        display(image.previous());
     }
+
 }
