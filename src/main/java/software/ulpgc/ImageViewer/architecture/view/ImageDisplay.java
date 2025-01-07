b845e32f68c17e9938f8e08a72186690ead1b241
TychoQS <tycho.quintana@gmail.com>
2025-01-07 14:32:35 +0000

    We want to support zoom functionality in our application so we need to add these method to the ImageDisplay interface hence all ImageDisplay subclasses give us a form to communicate from the presenter what to do when zooming

--- src/main/java/software/ulpgc/ImageViewer/architecture/view/ImageDisplay.java
+++ src/main/java/software/ulpgc/ImageViewer/architecture/view/ImageDisplay.java
@@ -3,9 +3,11 @@ package software.ulpgc.ImageViewer.architecture.view;
 import software.ulpgc.ImageViewer.architecture.model.Image;
 
 public interface ImageDisplay {
-    void display(Image image, int offset);
+    void display(Image image, int offset, double zoomFactor);
     void onReleasing(Released released);
     void onDragging(Dragged dragged);
+    void onZooming(Zoomed zoomed);
+    boolean isBeingDragged();
     int getWidth();
     void clear();
     interface Dragged {
@@ -15,5 +17,9 @@ public interface ImageDisplay {
     interface Released {
         Released Null = position -> {};
         void position (int position);
+    }
+    interface Zoomed {
+        Zoomed Null = position -> {};
+        void increase(int increment);
     }
 }
